474ef28fe93dfddbf3cfdf9470fa1b23f0cba0e3
Carlos Henrique <carloshenriquechhp@gmail.com>
2019-06-26 16:19:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 jQuery(document).ready(function($) {
 
 	// console.log("DOM carregado");
+	
+	$(".lg-hotspot-label").show("fast");
+	$(".lg-hotspot-label").hide();
+
+	$(".lg-hotspot-button").click(function(){
+	var thisLabel = $(this).siblings(".lg-hotspot-label");
+	var thisLabelState = thisLabel.css("display");
+	$(".lg-hotspot-label").fadeOut(0).parent().css("z-index", "0");
+	if(thisLabelState=="none") {
+	thisLabel.fadeIn(0);
+	$(this).parent().css("z-index", "999");
+	 }
+	});
 
 	$(".hotspot").each(function(){
 
