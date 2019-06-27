f329026156b596caae7063a2580f8cb252d126b3
Carlos Henrique <carlos@papricacomunicacao.com.br>
2019-06-27 00:29:50 -0300

    [Change] Adicionado hotspots; Refinamento aparencia

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
 jQuery(document).ready(function($) {
 
-	// console.log("DOM carregado");
+	console.log("DOM carregado");
 	
-	$(".lg-hotspot-label").show("fast");
-	$(".lg-hotspot-label").hide();
+	// $(".lg-hotspot-label").show("fast");
+	// $(".lg-hotspot-label").hide();
 
-	$(".lg-hotspot-button").click(function(){
-	var thisLabel = $(this).siblings(".lg-hotspot-label");
-	var thisLabelState = thisLabel.css("display");
-	$(".lg-hotspot-label").fadeOut(0).parent().css("z-index", "0");
-	if(thisLabelState=="none") {
-	thisLabel.fadeIn(0);
-	$(this).parent().css("z-index", "999");
-	 }
-	});
+	// $(".lg-hotspot-button").click(function(){
+	// var thisLabel = $(this).siblings(".lg-hotspot-label");
+	// var thisLabelState = thisLabel.css("display");
+	// $(".lg-hotspot-label").fadeOut(0).parent().css("z-index", "0");
+	// if(thisLabelState=="none") {
+	// thisLabel.fadeIn(0);
+	// $(this).parent().css("z-index", "999");
+	//  }
+	// });
 
-	$(".hotspot").each(function(){
+	// $(".hotspot").each(function(){
 
-		var $this = $(this),
-		top = $this.data("top"),
-		left = $this.data("left");
+	// 	var $this = $(this),
+	// 	top = $this.data("top"),
+	// 	left = $this.data("left");
 
-		$this.css({
-			top: top + "%",
-			left: left + "%"
-		})
-		.addClass("is-visible");
+	// 	$this.css({
+	// 		top: top + "%",
+	// 		left: left + "%"
+	// 	})
+	// 	.addClass("is-visible");
 
-	});
+	// });
 
-	$(".hotspots-label").on("mouseover", function(e){
-		$(this).removeClass("is-visible");
-		$(this).parents(".image").find(".hotspot").removeClass("is-active");
-		e.preventDefault();
-	});
+	// $(".hotspots-label").on("mouseover", function(e){
+	// 	$(this).removeClass("is-visible");
+	// 	$(this).parents(".image").find(".hotspot").removeClass("is-active");
+	// 	e.preventDefault();
+	// });
 
-	$(".hotspot").on("mouseover", function(e){
+	// $(".hotspot").on("mouseover", function(e){
 
-		var text = $(this).data("text");
+	// 	var text = $(this).data("text");
 
-		if(!$(this).hasClass("is-active"))
-		{
-			$(this).parents(".image").find(".hotspot").removeClass("is-active");
-			$(this).addClass("is-active");
-			$(this).parents(".image").find(".hotspots-label").html( "<strong>" + $(this).text() + "</strong> <span>" + text + "</span>" ).addClass("is-visible");
-		}
-		else
-		{
-			$(this).removeClass("is-active");
-			$(this).parents(".image").find(".hotspots-label").html( "<strong>" + $(this).text() + "</strong> <span>" + text + "</span>" ).removeClass("is-visible");	
-		}
+	// 	if(!$(this).hasClass("is-active"))
+	// 	{
+	// 		$(this).parents(".image").find(".hotspot").removeClass("is-active");
+	// 		$(this).addClass("is-active");
+	// 		$(this).parents(".image").find(".hotspots-label").html( "<strong>" + $(this).text() + "</strong> <span>" + text + "</span>" ).addClass("is-visible");
+	// 	}
+	// 	else
+	// 	{
+	// 		$(this).removeClass("is-active");
+	// 		$(this).parents(".image").find(".hotspots-label").html( "<strong>" + $(this).text() + "</strong> <span>" + text + "</span>" ).removeClass("is-visible");	
+	// 	}
 
-		e.preventDefault();
-	});
+	// 	e.preventDefault();
+	// });
 
-	$(".hotspot").on("mouseleave", function(e){
-		var text = $(this).data("text");
+	// $(".hotspot").on("mouseleave", function(e){
+	// 	var text = $(this).data("text");
 
-		$(this).removeClass("is-active");
-		$(this).parents(".image").find(".hotspots-label").html( "<strong>" + $(this).text() + "</strong> <span>" + text + "</span>" ).removeClass("is-visible");
-	});
+	// 	$(this).removeClass("is-active");
+	// 	$(this).parents(".image").find(".hotspots-label").html( "<strong>" + $(this).text() + "</strong> <span>" + text + "</span>" ).removeClass("is-visible");
+	// });
 
 });
